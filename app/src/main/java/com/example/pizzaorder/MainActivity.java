32f9bd725814bad8b8b934bb25a1d860c8a019dc
Irish <irishrocha4@gmail.com>
2025-05-05 21:18:43 +0800

    xml layout code...

---
+++ app/src/main/java/com/example/pizzaorder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pizzaorder;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //code.....
+}
